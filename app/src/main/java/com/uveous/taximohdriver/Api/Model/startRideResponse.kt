4e6ca9bb64faa0a93c5258ae03e5dd0bdb0196a4
ankitsingh <Shaluch08>
2021-05-10 18:05:53 +0530

    Initial commit

---
+++ app/src/main/java/com/uveous/taximohdriver/Api/Model/startRideResponse.kt
@@ -0,0 +1,10 @@
+package com.uveous.loopfoonpay.Api.Model
+
+import com.google.gson.annotations.SerializedName
+
+data class startRideResponse(
+    @SerializedName("status")
+    var status:Int
+
+
+)
